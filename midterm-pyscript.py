3fba638039f23de2cc2423b435dc12bb86f9e56e
Jesse Gilbert <32143901+jesenator@users.noreply.github.com>
2023-11-05 14:03:51 -0500

    Update midterm-pyscript.py

--- midterm-pyscript.py
+++ midterm-pyscript.py
@@ -1,69 +1,77 @@
 cv2_image = cv2.cvtColor(np.array(cam.raw_image), cv2.COLOR_RGB2HSV)
 cv2_image_RGB = cv2.cvtColor(cv2_image, cv2.COLOR_HSV2RGB)
 
-lower = np.array([0, 100, 100])
-upper = np.array([70, 255, 255])
+colors = ["red", "green"]
+mag = 90
+lower_bound = 50
+color_one_count = 0
+color_two_count = 0
+for color in colors:
+    if color == "red":
+        lower_red1 = np.array([0, lower_bound, lower_bound])
+        upper_red1 = np.array([0 + mag, 255, 255])
+        lower_red2 = np.array([180 - mag, lower_bound, lower_bound])
+        upper_red2 = np.array([180, 255, 255])
+        mask1 = cv2.inRange(cv2_image, lower_red1, upper_red1)
+        mask2 = cv2.inRange(cv2_image, lower_red2, upper_red2)
+        mask = cv2.bitwise_or(mask1, mask2)
+    if color == "green":
+        lower = np.array([60 - mag, lower_bound, lower_bound])
+        upper = np.array([60 + mag, 255, 255])
+        mask = cv2.inRange(cv2_image, lower, upper)
 
-mask = cv2.inRange(cv2_image, lower, upper)
-display(Image.fromarray(cv2_image_RGB))
+    display(Image.fromarray(cv2_image_RGB))
 
-kernel = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]], dtype=np.uint8)
-# display(Image.fromarray(mask))
+    kernel = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]], dtype=np.uint8)
+    # display(Image.fromarray(mask))
 
-eroded = cv2.erode(mask, kernel)
-# display(Image.fromarray(eroded))
-dilated = cv2.dilate(eroded, kernel)
-# display(Image.fromarray(dilated))
+    eroded = cv2.erode(mask, kernel)
+    # display(Image.fromarray(eroded))
+    dilated = cv2.dilate(eroded, kernel)
+    # display(Image.fromarray(dilated))
 
-masked = cv2.bitwise_and(cv2_image_RGB, cv2_image_RGB, mask=dilated)
-display(Image.fromarray(masked))
-count = np.count_nonzero(dilated)
-print(count)
-if count > 500:
-    print("updating airtable...")
+    masked = cv2.bitwise_and(cv2_image_RGB, cv2_image_RGB, mask=dilated)
+    display(Image.fromarray(masked))
+    count = np.count_nonzero(dilated)
+    print(count)
+    if color == "red":
+        color_one_count = count
+    if color == "green":
+        color_two_count = count
 
-    api_key = 'patHUTHeOmbhBkd56.96a8e2afeea480d41aa27eeb9b01ee74afdd9476994d6219b82dbce8bd67ac73'
-    base_id = 'appiqDH9yo5f9lCwC'
-    table_name = 'tblzIa6NVYGyQBPdr'
-    record_id = 'rec4JEibPBPPGQJI6'
+new_val = "F" if color_one_count > color_two_count else "C"
+print(f"updating airtable... to {new_val}")
+from pyodide.http import pyfetch
+import json
+import asyncio
 
-    # Headers to authenticate
+
+async def patch_record(base_id, table_name, record_id, api_key):
+    endpoint = f'https://api.airtable.com/v0/{base_id}/{table_name}/{record_id}'
+    data = {
+        "fields": {
+            "Name": new_val,
+        },
+    }
     headers = {
         'Authorization': f'Bearer {api_key}',
         'Content-Type': 'application/json'
     }
 
-    #################### getting record name
-    endpoint = f'https://api.airtable.com/v0/{base_id}/{table_name}'
-    response = requests.get(endpoint, headers=headers)
+    response = await pyfetch(
+        url=endpoint,
+        method='PATCH',
+        headers=headers,
+        body=json.dumps(data),
+    )
+    result = await response.json()
+    print(result)
 
-    # Check if the request was successful
-    if response.status_code == 200:
-        # Parse the response as JSON
-        records = response.json().get('records', [])
-        for record in records:
-            print(record)
-            id = record['id']
-            print(id)
-            name = record['fields']['Name']
-            print(name)
-    else:
-        print(f'Error: {response.status_code}')
 
-    #################### updating record name
-    # endpoint = f'https://api.airtable.com/v0/{base_id}/{table_name}/{record_id}'
+api_key = 'patHUTHeOmbhBkd56.96a8e2afeea480d41aa27eeb9b01ee74afdd9476994d6219b82dbce8bd67ac73'
+base_id = 'appiqDH9yo5f9lCwC'
+table_name = 'tblzIa6NVYGyQBPdr'
+record_id = 'rec4JEibPBPPGQJI6'
 
-    # data = {
-    #     "fields": {
-    #         "Name": "Ferry lkjnBuilding",
-    #     },
-    # }
-    # response = requests.patch(endpoint, headers=headers, data=json.dumps(data))
-    # if response.status_code == 200:
-    #     print('Successfully updated the record.')
-    #     updated_record = response.json()
-    #     print(json.dumps(updated_record, indent=4))
-    # else:
-    #     print(f'Failed to update the record: {response.status_code}')
-    #     print(response.json())
-
+# Schedule the coroutine to run
+asyncio.ensure_future(patch_record(base_id, table_name, record_id, api_key))
